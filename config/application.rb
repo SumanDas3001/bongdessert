d33353b8e22b98028c00602b3c37c7566f47bd5b
SumanDas3001 <sumandas3001@gmail.com>
2022-11-15 19:48:19 +0530

    add secret

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Bongdessert
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.secret_key_base = '7938a7da8c41e899c04ba7439be1d55f841f10d1d879bf6d94444d8049cec5bca3f2f4391fc060894eadc208393a27d8336cafe4d626af67eb68d499f9155829'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
